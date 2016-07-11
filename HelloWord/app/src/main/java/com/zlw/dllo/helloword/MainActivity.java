0c8946b266130cdbebb386ad6a2aa05a5a3aa4e4
zhongliwei <1291799713@qq.com>
2016-07-11 11:29:20 +0800

    功能1开始执行

--- HelloWord/app/src/main/java/com/zlw/dllo/helloword/MainActivity.java
+++ HelloWord/app/src/main/java/com/zlw/dllo/helloword/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // 功能1 开始敲代码 啪啪啪拍照
     }
 }
